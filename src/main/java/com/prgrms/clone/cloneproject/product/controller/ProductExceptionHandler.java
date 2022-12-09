3028b9ca4fc26cfafff1767517c592d3c015fcb7
Surin Lee <leesurin99@gmail.com>
2022-12-10 01:59:35 +0900

    refactor: 인증 및 중복 가입 예외 처리 구현

--- src/main/java/com/prgrms/clone/cloneproject/product/controller/ProductExceptionHandler.java
+++ src/main/java/com/prgrms/clone/cloneproject/product/controller/ProductExceptionHandler.java
@@ -1,8 +1,9 @@
 package com.prgrms.clone.cloneproject.product.controller;
 
+import com.prgrms.clone.cloneproject.customer.exception.DuplicateUserException;
+import com.prgrms.clone.cloneproject.customer.exception.NoAuthenticatedException;
 import com.prgrms.clone.cloneproject.data.ErrorResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.prgrms.clone.cloneproject.order.service.NoOrderException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,12 +13,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ProductExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(ProductExceptionHandler.class);
-
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResult> handleMethodArgumentNotValidException(HttpMessageNotReadableException httpMessageNotReadableException) {
-        String errorDetail = "JSON 값이 유효하지 않습니다.";
-        ErrorResult errorResult = new ErrorResult("데이터 형태 오류", errorDetail);
+        ErrorResult errorResult = new ErrorResult("데이터 형태 오류", httpMessageNotReadableException.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
     }
@@ -25,6 +23,27 @@ public class ProductExceptionHandler {
     @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
     public ResponseEntity<ErrorResult> handleInvalidParameterException(Exception exception) {
         ErrorResult errorResult = new ErrorResult("Illegal Exception", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
+    }
+
+    @ExceptionHandler(NoOrderException.class)
+    public ResponseEntity<ErrorResult> handleNoOrderException(NoOrderException noOrderException) {
+        ErrorResult errorResult = new ErrorResult("No order", noOrderException.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResult);
+    }
+
+    @ExceptionHandler(NoAuthenticatedException.class)
+    public ResponseEntity<ErrorResult> handleNoAuthentivatedException(NoAuthenticatedException noAuthenticatedException) {
+        ErrorResult errorResult = new ErrorResult("Not valid cuatomer", noAuthenticatedException.getMessage());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResult);
+    }
+
+    @ExceptionHandler(DuplicateUserException.class)
+    public ResponseEntity<ErrorResult> handleDulplicateException(DuplicateUserException duplicateUserException) {
+        ErrorResult errorResult = new ErrorResult("중복값입니다.", duplicateUserException.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
     }
